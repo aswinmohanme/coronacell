8fcc2ebd1048a801a69921c13a4ee1a724c2f5d3
aswinmohanme <dev.aswinmohan@gmail.com>
2020-04-16 14:59:38 +0530

    Doctors are awesome

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_15_095632) do
+ActiveRecord::Schema.define(version: 2020_04_16_085647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -113,6 +113,7 @@ ActiveRecord::Schema.define(version: 2020_04_15_095632) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "role"
     t.bigint "panchayat_id"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["panchayat_id"], name: "index_users_on_panchayat_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
